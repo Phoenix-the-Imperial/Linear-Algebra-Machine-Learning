420b0f7772ad0e9caa3c30102dbe20a406fa401a
Phoenix-the-Imperial <www.tennysonbenjamin@gmail.com>
2023-12-30 23:45:07 +0530

    Version ML-0.0.1 Update!

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Machine Learning
+## Description
+ A Machine Learning library written in C++. Current Version: ML-0.0.1.
+## Version: ML-0.0.1
+ Defined a basic matrix class.
 ## Version: ML-0.0.0
-A Machine Learning library written in C++.
+ Only the rough sketches for the matrix class and vector dot product added.
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 // include <linear_regression.hpp>
 #include <matrix.hpp>
-
+// #include <vector>
 int main()
 {
-    auto m = ML::matrix({1, 2, 3, 4, 5, 6}, 2, 3);
+    // std::vector<std::vector<float>> v = {{1, 2, 3}, {1, 2}};
+    // v.at(0).resize(4);
+    // v.at(0).at(3) = 4;
+    // std::cout << v.at(0).at(3) << std::endl;
+    auto m = ML::matrix<float>({1, 2, 3, 4, 5, 6}, 2, 3);
     std::cout << m;
     return 0;
 }
